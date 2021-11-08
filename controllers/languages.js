6e19030ebccb1d89e4038ff807faac3c9a4594db
mu2ku <gmutuku@gmail.com>
2021-11-08 02:18:23 -0500

    commit initial languages index, create, and new controllers

--- controllers/languages.js
+++ controllers/languages.js
@@ -1,23 +1,31 @@
 import { Language } from "../models/language.js"
 
 function index(req,res){
+  Language.find({})
+  .then(languages => 
+    res.render('languages/index',{
+      title: "Language List",
+      languages
+  })
+)}
 
-}
-
-function show(req,res){
-
-}
 
 function create(req,res){
-
+  
 }
 
 function newLanguage(req,res){
-
+  res.render('languages/new', {
+    title: 'Add Language'
+  })
 }
 
 function deleteLanguage(req,res){
   
+}
+
+function show(req,res){
+
 }
 
 export{
--- routes/languages.js
+++ routes/languages.js
@@ -2,6 +2,9 @@ import { Router } from 'express'
 import * as langCtrl from '../controllers/languages.js'
 const router = Router()
 
+router.get('/', langCtrl.index)
+router.get('/new',langCtrl.new)
+
 export {
   router
 }
