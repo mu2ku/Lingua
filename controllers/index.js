c86cc4338c967fe3a2f294b99ed0724224d0a8ef
mu2ku <gmutuku@gmail.com>
2021-11-11 02:32:07 -0500

    commit functioning profile resources

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,18 @@
 import { Resource } from '../models/resource.js'
+import { Language } from '../models/language.js'
 
 function index(req,res){
   Resource.find({})
   .sort({_id: -1})
-  .limit(10)
+  .limit(6)
   .then (resources => {
-    res.render('index',{
-      resources,
-      title: 'Latest Activity',
-      user: req.user ? req.user : null
+    Language.find({})
+    .then (languages => {
+      res.render('index',{
+        resources,
+        title: 'Latest Activity',
+        user: req.user ? req.user : null
+      })
     })
   })
 }
