69f040d3e07f959012fe9e327e30633a5c38d6c6
mu2ku <gmutuku@gmail.com>
2021-11-11 18:25:56 -0500

    commit show view for all favorited resources

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -7,9 +7,33 @@ function show(req,res){
   .then(profile => {
     Profile.findById(req.user.profile)
     .then(userProfile => {
-      Resource.find({ collectedBy: req.user.profile._id }).limit(10)
+      Resource.find({ collectedBy: req.user.profile._id }).limit(4)
       .then(resources => {
         res.render('profiles/show',{
+          title: 'Profile',
+          profile,
+          userProfile,
+          user: req.user ? req.user : null,
+          resources
+        })
+      })
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function showFavorites(req,res){
+  Profile.findById(req.params.id)
+  .populate('collectedResources')
+  .then(profile => {
+    Profile.findById(req.user.profile)
+    .then(userProfile => {
+      Resource.find({ collectedBy: req.user.profile._id })
+      .then(resources => {
+        res.render('profiles/resources', {
           title: 'Title',
           profile,
           userProfile,
@@ -26,5 +50,6 @@ function show(req,res){
 }
 
 export{
-  show
+  show,
+  showFavorites
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -3,6 +3,7 @@ import * as profilesCtrl from '../controllers/profiles.js'
 const router = Router()
 
 router.get('/:id', profilesCtrl.show)
+router.get('/:id/resources', profilesCtrl.showFavorites)
 
 export {
   router
